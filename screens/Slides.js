748588996a7935e5de703805d61ebfa2b880aec3
laksamanaap <laksamana.arya1412@gmail.com>
2024-03-04 23:18:14 +0700

    feat : finalize photogood app (war is over). Semoga besok lancar amiiiiinnn

--- screens/Slides.js
+++ screens/Slides.js
@@ -8,20 +8,23 @@ export default [
   },
   {
     id: "2",
-    image: require("../assets/images/slides-2.gif"),
+    image: require("../assets/images/slides-7.gif"),
     title: "Unggah dan Simpan Foto",
-    description: "Abadikan momen berharga kalian di photogood",
+    description: "Abadikan momen berharga di hidup kalian hanya di Photogood",
   },
   {
     id: "3",
     image: require("../assets/images/slides-4.gif"),
+    // image: require("../assets/images/slides-4.gif"),
     title: "Forum Diskusi",
-    description: "Bangun komunikasi yang antar pengguna photogood",
+    description:
+      "Bangun komunikasi dengan cara berdiskusi antar pengguna antar pengguna Photogood",
   },
   {
     id: "4",
     image: require("../assets/images/slides-5.gif"),
     title: "Membership",
-    description: "Berlangganan untuk mendapatkan fitur yang lebih lengkap",
+    description:
+      "Berlangganan untuk mendapatkan fitur yang lebih lengkap dari aplikasi kami",
   },
 ];
